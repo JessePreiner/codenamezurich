ecf280b1b5b0ff4022dfc3d641d66d67cb3d9442
Jesse Preiner <jezzah@gmail.com>
2016-11-23 16:56:49 -0600

    initial firebase setup. support gmail auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable, AuthMethods, AuthProviders } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(public af: AngularFire) {
+    this.af.auth.subscribe(auth => console.log(auth));
+  }
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule, AuthProviders, AuthMethods  } from 'angularfire2';
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyCQYbUtadvTq3Po9t858QLIVIl_ros1qnk",
+    authDomain: "codenamezurich.firebaseapp.com",
+    databaseURL: "https://codenamezurich.firebaseio.com",
+    storageBucket: "codenamezurich.appspot.com",
+    messagingSenderId: "350319442557"
+};
+
+const authConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +24,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig, authConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
